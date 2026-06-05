7b3b7cb4d2fcfa33a6cfb3260d4391305af2465c
BackUpSystem42 <arcoiris.monocromatico.25@gmail.com>
2026-06-05 14:28:34 -0400

    Base Assets Additions

--- Assets/JavaScript/FetchElements.js
+++ Assets/JavaScript/FetchElements.js
@@ -17,10 +17,7 @@
 //   });
 
 GetComponent('BoilerPlate/NavBar.html','navbar-placeholder').then(result => document.getElementById('navbar-placeholder').innerHTML = result);
-// GetComponent('BoilerPlate/LogoBanner.html','logobanner-placeholder');
 
-// @param {string} location
-// @param {string} id
 async function GetComponent(location, id)
 {
 // const response = await
@@ -41,16 +38,5 @@ async function GetComponent(location, id)
     console.error('Error loading navigation:', error);
   });
 
-
-
-    // Check if the request was successful
-//     if (!response.ok) {
-//       throw new Error(`Failed to load nav: ${response.status}`);
-//     }
-//      const result= await response.text(); // Convert response to text
-// return result;
-  
-    // Insert the navigation HTML into the placeholder
-    // document.getElementById(id).innerHTML = data;
  
 }
